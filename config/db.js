60054a804734ea9f0fc797dbb81b64b510aca87a
Bryson <brysonttaylor@gmail.com>
2021-02-10 18:58:33 -0800

    implemented mongoDB and .env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const DB_URL = 'mongodb+srv://onion:redwood2016@auth-api.ufkkb.mongodb.net/users?retryWrites=true&w=majority';
+const DB_URL = process.env.DB_URL;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect.once('open', () => console.log(`Connected to mongo DB at ${DB_URL}!`));
+mongoose.connection.once('open', () => console.log(`Connected to mongo DB at ${DB_URL}!`));
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+// Database setup
+require('dotenv').config();
+require('./config/db');
 
 // --- MIDDLE WARE ---
 app.use(express.json());
