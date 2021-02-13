477b8216b33242e5f2dba5d0844d83e336f86206
Bryson <brysonttaylor@gmail.com>
2021-02-13 10:13:08 -0800

    encrypted passwords

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,17 @@
 const User = require('../models/User');
 const { users } = require('../data/users');
 
-const seedUsers = (req, res) => {
-  User.create(users)
+const { encryptPassword } = require('../utils/encryption');
+
+const seedUsers = async (req, res) => {
+
+  const hashedUsers = [];
+
+  for (let i = 0; i < users.length; i++) {
+    hashedUsers[i] = { ...users[i], password: await encryptPassword(users[i].password) };
+  }
+
+  User.create(hashedUsers)
     .then(users => res.status(200).send(users))
     .catch(err => res.status(500).send({ Error: err.message }));
 };
